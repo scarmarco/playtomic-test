875b110bf01aac43dc79ed2e6709227ab952ba09
Marco Scarciofolo <scar_marco@hotmail.es>
2024-12-19 00:38:31 +0100

    Login, logout and persistance implementation

--- src/lib/auth/AuthProvider.tsx
+++ src/lib/auth/AuthProvider.tsx
@@ -1,5 +1,21 @@
-import { ReactNode } from 'react'
-import { AuthInitializeConfig } from './types'
+import {
+  ReactNode,
+  createContext,
+  useState,
+  useEffect,
+  useCallback,
+} from 'react'
+import { useApiFetcher } from '@/lib/api'
+import { Auth, AuthInitializeConfig, TokensData } from './types'
+
+interface AuthContextType {
+  tokens: Auth['tokens']
+  currentUser: Auth['currentUser']
+  saveTokens: AuthInitializeConfig['onAuthChange']
+  setCurrentUser: (user: Auth['currentUser']) => void
+}
+
+const AuthContext = createContext<AuthContextType | null>(null)
 
 interface AuthProviderProps extends AuthInitializeConfig {
   children?: ReactNode
@@ -23,10 +39,64 @@ interface AuthProviderProps extends AuthInitializeConfig {
  */
 function AuthProvider(props: AuthProviderProps): JSX.Element {
   const { initialTokens, onAuthChange, children } = props
+  const [tokens, setTokens] = useState<Auth['tokens']>(null)
+  const [currentUser, setCurrentUser] = useState<Auth['currentUser']>(undefined)
+  const fetcher = useApiFetcher()
+
+  const saveTokens = useCallback(
+    (tokens: TokensData | null) => {
+      setTokens(tokens)
+      onAuthChange && onAuthChange(tokens)
+    },
+    [onAuthChange]
+  )
+
+  useEffect(() => {
+    const loadInitialTokens = async () => {
+      const storedTokens = await initialTokens
+
+      if (storedTokens) {
+        saveTokens(storedTokens)
+        try {
+          const userResponse = await fetcher(
+            'GET /v1/users/me',
+            {},
+            {
+              headers: {
+                authorization: `Bearer ${storedTokens.access}`,
+              },
+            }
+          )
+
+          if (userResponse.ok) {
+            const { userId, email, displayName } = userResponse.data
+            setCurrentUser({ userId, email, name: displayName })
+          }
+        } catch (error) {
+          console.error(error)
+        }
+      } else {
+        setCurrentUser(null)
+      }
+    }
+
+    loadInitialTokens().catch((error) => {
+      console.error('Load initial tokens failed', error)
+    })
+  }, [fetcher, initialTokens, saveTokens])
 
   return (
-    <>{children}</>
+    <AuthContext.Provider
+      value={{
+        tokens,
+        currentUser,
+        saveTokens,
+        setCurrentUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
   )
 }
 
-export { AuthProvider, type AuthProviderProps }
+export { AuthProvider, type AuthProviderProps, AuthContext }
--- src/lib/auth/useAuth.ts
+++ src/lib/auth/useAuth.ts
@@ -1,4 +1,7 @@
+import { useContext } from 'react'
+import { useApiFetcher } from '@/lib/api'
 import { Auth } from './types'
+import { AuthContext } from './AuthProvider'
 
 /**
  * Returns the current auth state. See {@link Auth} for more information on
@@ -7,15 +10,59 @@ import { Auth } from './types'
  * @throws {TypeError} if called from a component not descendant of AuthProvider
  */
 function useAuth(): Auth {
+  const fetcher = useApiFetcher()
+  const authContext = useContext(AuthContext)
+
+  if (!authContext) {
+    throw new TypeError('useAuth must be used within a AuthProvider')
+  }
+
+  const { tokens, currentUser, saveTokens, setCurrentUser } = authContext
+
   return {
-    tokens: null,
-    currentUser: null,
-    login(credentials) {
+    tokens,
+    currentUser,
+    async login(credentials) {
       const { email, password } = credentials
-      return Promise.reject(new Error('Not yet implemented'))
+
+      try {
+        const authResponse = await fetcher('POST /v3/auth/login', {
+          data: { email, password },
+        })
+
+        if (!authResponse.ok) {
+          return Promise.reject(new Error(authResponse.data.message))
+        }
+
+        saveTokens?.({
+          access: authResponse.data.accessToken,
+          accessExpiresAt: authResponse.data.accessTokenExpiresAt,
+          refresh: authResponse.data.refreshToken,
+          refreshExpiresAt: authResponse.data.refreshTokenExpiresAt,
+        })
+
+        const userResponse = await fetcher(
+          'GET /v1/users/me',
+          {},
+          {
+            headers: {
+              authorization: `Bearer ${authResponse.data.accessToken}`,
+            },
+          }
+        )
+
+        if (userResponse.ok) {
+          const { userId, email: userEmail, displayName } = userResponse.data
+          setCurrentUser({ userId, email: userEmail, name: displayName })
+        }
+      } catch (error) {
+        return Promise.reject(new Error(error as string))
+      }
     },
     logout() {
-      return Promise.reject(new Error('Not yet implemented'))
+      saveTokens?.(null)
+      setCurrentUser(null)
+      return Promise.resolve()
     },
   }
 }
